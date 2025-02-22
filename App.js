14abe5efd551ce36a2e99cd184437834af019b93
siestaspm <157125129+siestaspm@users.noreply.github.com>
2025-02-22 11:16:19 +0800

    update functions

--- App.js
+++ App.js
@@ -1,54 +1,56 @@
 import React, {useState} from 'react';
-import { Text, View, Image, TextInput, Button, TouchableOpacity, FlatList, ScrollView } from 'react-native';
-import { styles } from './src/styles/MainStyle';
+import { Text, View, Image,TextInput, TouchableOpacity } from 'react-native';
+import { loginstyle } from './src/styles/MainStyle';
 
-
-const EmployeeNames = ({item, setData}) => { 
-  
-  const handleDelete = () => {
-    setData(prev => prev.filter(name => name.id !== item.id))
-  }
-  return (
-    <View style={styles.FlatListContainer}>
-      <Text style={styles.FlatListTextStyle}>
-        {item.name}
-      </Text>
-      <Text style={styles.FlatListTextStyle}>
-        {item.type}
-      </Text>
-      <Button
-        style={styles.DeleteButton}
-        title='Delete Me!'
-        onPress={handleDelete}
-      />
-  </View>
-  );
-
-}
 const App = () => {
-  const [username, setUsername] = useState('');
-  const [workToDo, setWorkToDo] = useState('');
-  const [count, setCount] = useState(0);
-  const [data, setData] = useState([]);
+  const [username, setUsername] = useState('')
+  const [firstName, setFirstName] = useState('')
 
-  const handleSave = () => { 
-    setData(prev => [...prev,{id: prev.length + 1, name: username, type: workToDo}])
-    setUsername('')
-    setWorkToDo('')
-    console.log(data)
+  const handlePress = () => { 
+    console.log('hello')
+    setUsername('Letran')
+    setFirstName('Sean')
   }
+
   return (
-    <View style={styles.container}>
+    <View style={loginstyle.container}>
+      
+      <TextInput 
+      value={username}
+      style={loginstyle.TextInput}
+      onChangeText={(text) => setUsername(text)}
+      />
+      <TextInput 
+      value={firstName}
+      style={loginstyle.TextInput}
+      onChangeText={(text) => setFirstName(text)}
+      />
+      <TouchableOpacity
+      onPress={handlePress}
+      >
+       <Image
+      style={loginstyle.ImageContainer}
+      source={require('./src/assets/Breadcoat-British-Shorthair.jpg')}
+      />
+      </TouchableOpacity>
+    <Text>
+      Hello CL7
+    </Text>
+    </View>
+
+  );
+};
+
+export default App;
             {/* <TouchableOpacity
       onPress={() => setCount(prev => prev + 1)}>
-      <Image
-        style={styles.ImageContainer}
-        source={{
-          uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg==',
-        }}
-      />
+      
       </TouchableOpacity> */}
-      <TextInput
+      {/* <Image 
+      style={styles.ImageContainer}
+      source={require('./src/assets/Breadcoat-British-Shorthair.jpg')}
+      /> */}
+      {/* <TextInput
       style={styles.TextInput}
       value={username}
       onChangeText={(text)=> setUsername(text)}/>
@@ -61,23 +63,16 @@ const App = () => {
       <Button
       style={styles.SuccessButton}
       title='Click Me!'
-      onPress={handleSave}/>
+      onPress={handleSave}/> */}
 
-
+{/* 
     <FlatList
       data={data}
       renderItem={({ item }) => <EmployeeNames item={item} setData={setData}/>} 
-      initialNumToRender={1}
-      windowSize={5}
       keyExtractor={(item, index) => index.toString()} 
       style={{
         flexGrow: 0,
         width: 100,
         alignSelf: 'center',
         marginBottom: 25
-    }}/>
-    </View>
-  );
-};
-
-export default App;
+    }}/> */}
