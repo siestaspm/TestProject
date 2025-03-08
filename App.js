4206c6f025e85fe31bf96016c0dc0b34bd6cf265
siestaspm <157125129+siestaspm@users.noreply.github.com>
2025-03-08 10:39:32 +0800

    udpate final

--- App.js
+++ App.js
@@ -3,43 +3,14 @@ import { Text, View, Image, TextInput, Button, TouchableOpacity, FlatList, Scrol
 import { styles } from './src/styles/MainStyle';
 
 
-const EmployeeNames = ({item, setData}) => { 
-  
-  const handleDelete = () => {
-    setData(prev => prev.filter(name => name.id !== item.id))
-  }
-  return (
-    <View style={styles.FlatListContainer}>
-      <Text style={styles.FlatListTextStyle}>
-        {item.name}
-      </Text>
-      <Text style={styles.FlatListTextStyle}>
-        {item.type}
-      </Text>
-      <Button
-        style={styles.DeleteButton}
-        title='Delete Me!'
-        onPress={handleDelete}
-      />
-  </View>
-  );
-
-}
 const App = () => {
   const [username, setUsername] = useState('');
   const [workToDo, setWorkToDo] = useState('');
   const [count, setCount] = useState(0);
   const [data, setData] = useState([]);
-
-  const handleSave = () => { 
-    setData(prev => [...prev,{id: prev.length + 1, name: username, type: workToDo}])
-    setUsername('')
-    setWorkToDo('')
-    console.log(data)
-  }
   return (
     <View style={styles.container}>
-            {/* <TouchableOpacity
+            <TouchableOpacity
       onPress={() => setCount(prev => prev + 1)}>
       <Image
         style={styles.ImageContainer}
@@ -47,7 +18,7 @@ const App = () => {
           uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg==',
         }}
       />
-      </TouchableOpacity> */}
+      </TouchableOpacity>
       <TextInput
       style={styles.TextInput}
       value={username}
@@ -58,24 +29,7 @@ const App = () => {
       value={workToDo}
       onChangeText={(text)=> setWorkToDo(text)}/>
 
-      <Button
-      style={styles.SuccessButton}
-      title='Click Me!'
-      onPress={handleSave}/>
-
-
-    <FlatList
-      data={data}
-      renderItem={({ item }) => <EmployeeNames item={item} setData={setData}/>} 
-      initialNumToRender={1}
-      windowSize={5}
-      keyExtractor={(item, index) => index.toString()} 
-      style={{
-        flexGrow: 0,
-        width: 100,
-        alignSelf: 'center',
-        marginBottom: 25
-    }}/>
+  
     </View>
   );
 };
